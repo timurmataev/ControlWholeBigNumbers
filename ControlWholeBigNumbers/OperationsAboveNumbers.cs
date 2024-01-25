3fb9c0a5e03a9d1bb4158f0e8e5727e605fa7b99
timurmataev <150360439+timurmataev@users.noreply.github.com>
2024-01-25 16:40:31 +0600

    Добавьте файлы проекта.

---
+++ ControlWholeBigNumbers/OperationsAboveNumbers.cs
@@ -0,0 +1,30 @@
+﻿namespace ControlWholeBigNumbers
+{
+    public class OperationsAboveNumbers
+    {
+        public void AdditionBigNumbers(long value1, long value2)
+        {
+            long result = value1 + value2;
+            Console.WriteLine($"Результат сложения: {result}");
+        }
+
+        public void SubtractionBigNumbers(long value1, long value2)
+        {
+            long result = value1 - value2;
+            Console.WriteLine($"Результат вычитания: {result}");
+        }
+
+        public void MultiplicationBigNumbers(long value1, long value2)
+        {
+            long result = value1 * value2;
+            Console.WriteLine($"Результат умножения: {result}");
+        }
+
+        public void DivisionBigNumbers(long value1, long value2)
+        {
+            long result = value1 / value2;
+            Console.WriteLine($"Результат деления: {result}");
+        }
+
+    }
+}
---
+++ ControlWholeBigNumbers/Program.cs
@@ -0,0 +1,60 @@
+﻿using ControlWholeBigNumbers;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        OperationsAboveNumbers operationsAboveNumbers = new OperationsAboveNumbers();
+
+        Console.WriteLine("\tОперации с числами\n");
+
+        long userInputFirst = 0;
+        long userInputLast = 0;
+
+        Console.Write("Первое значение: ");
+        try
+        {
+            userInputFirst = Convert.ToInt64(Console.ReadLine());
+        }
+        catch
+        {
+            Console.WriteLine("Ввод должен быть числом");
+        }
+
+        Console.Write("Второе значение: ");
+        try
+        {
+            userInputLast = Convert.ToInt64(Console.ReadLine());
+        }
+        catch
+        {
+            Console.WriteLine("Ввод должен быть числом");
+        }
+
+        Console.WriteLine("\nСложение - 1\nВычитание - 2\nУмножение - 3\nДеление - 4\n");
+        ConsoleKeyInfo userKey = Console.ReadKey();
+        switch (userKey.Key)
+        {
+            case ConsoleKey.D1:
+                Console.WriteLine($"Команда: {userKey.Key}");
+                operationsAboveNumbers.AdditionBigNumbers(userInputFirst, userInputLast);
+                break;
+            case ConsoleKey.D2:
+                Console.WriteLine($"Команда: {userKey.Key}");
+                operationsAboveNumbers.SubtractionBigNumbers(userInputFirst, userInputLast);
+                break;
+            case ConsoleKey.D3:
+                Console.WriteLine($"Команда: {userKey.Key}");
+                operationsAboveNumbers.MultiplicationBigNumbers(userInputFirst, userInputLast);
+                break;
+            case ConsoleKey.D4:
+                Console.WriteLine($"Команда: {userKey.Key}");
+                operationsAboveNumbers.DivisionBigNumbers(userInputFirst, userInputLast);
+                break;
+            default:
+                Console.WriteLine("Такой команды нету");
+                break;
+        }
+
+    }
+}
